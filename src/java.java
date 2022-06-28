b8eadc14d54f6db88f6f9d836f1e2447e1efa448
raj <ponnamgangaraju9090@gmail.com>
2022-06-28 09:37:59 +0530

    My first commit

---
+++ src/java.java
@@ -0,0 +1,13 @@
+
+public class java {
+
+	public static void main(String[] args) {
+		 System.out.print("Hello world");
+		 System.out.print("Iam king");
+		 System.out.print("Hi India");
+		 
+		 
+
+	}
+
+}
